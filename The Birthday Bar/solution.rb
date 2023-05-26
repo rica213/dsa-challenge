b97d3227386f078ae65644ec68acfbd776a945a3
Tao (⌊arissa) <25f5k252l@mozmail.com>
2023-05-26 11:34:40 +0300

    Create solution for the birthday bar

---
+++ The Birthday Bar/solution.rb
@@ -0,0 +1,13 @@
+def birthday(s, d, m)
+    output = 0
+    start_point = 0
+    end_point = m-1
+    while end_point <= s.length-1
+        if s.slice(start_point,m).inject(:+) == d
+            output += 1
+        end
+        start_point +=1
+        end_point +=1
+    end 
+    output   
+end
