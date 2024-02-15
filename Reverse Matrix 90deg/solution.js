24c4324f6e3aa87dc7cd04157add624a87765306
Clarielle Larissa (Ƭao) <25f5k252l@mozmail.com>
2024-02-15 21:53:42 +0300

    Create solution.js

---
+++ Reverse Matrix 90deg/solution.js
@@ -0,0 +1,15 @@
+const ReverseMatrix90deg = (matrix) => {
+  const output = [];
+  for(let i = 0; i < matrix[0].length; i++) {
+    output[i] = [];
+    for(let j = matrix.length-1; j <= 0; j--) {
+      output[i].push(matrix[j][i]);
+    }
+  }
+  return output;
+}
+
+const matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
+console.log(ReverseMatrix90deg(matrix))
+
+//[[7, 4, 1],[8, 5, 2],[9, 6, 3]]
